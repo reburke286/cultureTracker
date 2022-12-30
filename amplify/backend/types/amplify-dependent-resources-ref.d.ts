e02efadbec9d799d9f7749db9b655800697a1118
reburke286 <reburke286@gmail.com>
2022-12-30 14:48:25 -0500

    configured amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "culturetracker9c16e76b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BooksPage, LoginForm, MoviesPage, MusicPage, TVPage } from "./pages";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
 function App() {
+  Amplify.configure(awsconfig);
   return (
     <div>
       <Router>
