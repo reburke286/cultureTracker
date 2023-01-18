ef17e862ac64e79b7ec76a347dd01a3c42e85ad5
reburke286 <reburke286@gmail.com>
2023-01-17 22:40:34 -0500

    edited book page table

--- src/pages/BooksPage/books-page.js
+++ src/pages/BooksPage/books-page.js
@@ -1,11 +1,18 @@
-import { DataTable, Piechart } from "../../components";
+import { useState, useEffect } from "react";
+import { DataTable, Piechart, Header } from "../../components";
 import { bookData } from "./2022_book_data";
-import { Rating } from "@mui/material";
+import { Rate } from "antd";
+import "./books-page.css";
 //`https://www.googleapis.com/books/v1/volumes?q=pride-and-protest&key=${process.env.GOOGLE_BOOKS_API}`
 
 /* TABLE DATA */
 const columns = [
-  { dataIndex: "title", title: "Title", key: "title" },
+  {
+    dataIndex: "title",
+    title: "Title",
+    key: "title",
+    sorter: (a, b) => a.title.localeCompare(b.title),
+  },
   {
     dataIndex: "author",
     key: "author",
@@ -22,20 +29,41 @@ const columns = [
     dataIndex: "review",
     title: "Rating",
     key: "rating",
-    sorter: (a, b) => a.review.localeCompare(b.review),
+    sorter: (a, b) => a.review - b.review,
     render: (value, record) => (
-      <Rating name="half-rating-read" precision={0.5} value={value} readOnly />
+      <Rate name="half-rating-read" precision={0.5} value={value} readOnly />
     ),
   },
 ];
 
 export const BooksPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (data.length === 0) {
+      const books = bookData.map((book) => {
+        return {
+          ...book,
+          key: book.title,
+        };
+      });
+      setData(books);
+    } else return;
+  }, [data]);
+
+  const handleBookModal = () => {
+    console.log("book modal");
+  };
+
   return (
-    <div>
-      <div>
-        <DataTable columns={columns} data={bookData} />
-        <Piechart />
-      </div>
+    <div className="booksWrapper">
+      {data.length === 0 ? null : (
+        <>
+          <Header pageTitle="Books" onClick={handleBookModal} />
+          <DataTable columns={columns} data={data} />
+        </>
+      )}
+      <Piechart />
     </div>
   );
 };
