45a6dfa56d8f9cc41406eaf438927c6e2b0ee02b
reburke286 <reburke286@gmail.com>
2023-01-17 22:40:23 -0500

    edited columns prop

--- src/components/DataTable/data-table.js
+++ src/components/DataTable/data-table.js
@@ -1,10 +1,14 @@
 import { Table } from "antd";
 import Stack from "@mui/material/Stack";
 
+import "./data-table.css";
+
 export const DataTable = ({ columns, data }) => {
   return (
-    <Stack>
-      <Table dataSource={data} columnSource={columns} />
-    </Stack>
+    <div className="datatable">
+      <Stack spacing={4}>
+        <Table dataSource={data} columns={columns} />
+      </Stack>
+    </div>
   );
 };
